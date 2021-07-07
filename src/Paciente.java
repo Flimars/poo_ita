52356b1a3d6d4b5206eed6e4ac96089b7ec3651f
Flimars <flaviolima.pop@gmail.com>
2021-07-06 21:26:37 -0300

    Status: Concluído.

--- src/Paciente.java
+++ src/Paciente.java
@@ -4,44 +4,27 @@ public class Paciente {
     double peso;
     double altura;
     double imc;
-
+// Construtor
     Paciente(double peso, double altura){
         this.peso   = peso;
         this.altura = altura;
     }
-
+// Método sem retorno = void 
     public void CalcularIMC(){
         imc = peso / (altura * altura);
     }
-
+// Método com retorno tipo String.
     public String Diagnostico(){
-        if (imc < 16){
-            return "Baixo peso muito grave";
-        }   
-        if (imc >= 16 || imc < 17){
-            return "Baixo peso grave";
-        }   
-        if (imc >= 17 || imc <= 18.49){
-            return "Baixo peso";
-        }   
-        if (imc >= 18.50 || imc <= 24.99){
-            return "Peso normal";
-        }   
-        if (imc >= 25 || imc <= 29.99){
-            return "Sobrepeso";
-        }   
-        if (imc >= 30 || imc <= 34.99){
-            return "Obesidade grau I";
-        }   
-        if (imc >= 35 || imc <= 39.99){
-            eturn "Obesidade grau II";
-        }   
-        if (imc >= 40){
-            return "Obesidade grau III (Obesidade mórbida)";
-        }
-        return null;  
         
-
+        if (imc < 16) return "Baixo peso muito grave";           
+        if (imc >= 16 && imc < 17) return "Baixo peso grave";    
+        if (imc >= 17 && imc < 18.50) return "Baixo peso";
+        if (imc >= 18.50 && imc < 25) return "Peso normal";
+        if (imc >= 25 && imc < 30) return "Sobrepeso";           
+        if (imc >= 30 && imc < 35) return "Obesidade grau I";           
+        if (imc >= 35 && imc < 40) return "Obesidade grau II";
+                
+        return "Obesidade grau III (Obesidade mórbida)";
     }
 
 }
