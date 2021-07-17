7aa09fa10a7b89958831ee844cc70936056abe3d
Flimars <flaviolima.pop@gmail.com>
2021-07-17 09:58:51 -0300

    Status: SemanaDois concluída. Start semanaTres.

---
+++ src/semanaDois/Somador.java
@@ -0,0 +1,17 @@
+package semanaII;
+
+public class Somador {
+    String nome;
+    int valorInstancia = 0;
+    static int valorEstatica = 0;
+
+    void somar(){
+        valorInstancia ++;
+        valorEstatica ++;
+    }
+
+    void imprimir(){
+        System.out.println("O somador "+ nome +" : instância = "+ valorInstancia +" e estatica = "+ valorEstatica);
+    }
+    
+}
